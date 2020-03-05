69584b2ff534594dc0b50b29741ce1d041e34a8e
brunolgb <brunodscolado@gmail.com>
2020-03-05 13:11:20 -0400

    Mensagem de erro estilizada e parametrizada

--- src/components/MessageErrors/index.js
+++ src/components/MessageErrors/index.js
@@ -3,16 +3,35 @@ import {
     View,
     Text
 } from 'react-native';
+import Style from './style'
 
 function MessageErrors(props){
+    const [progress, setProgress] = React.useState(0);
+
     const { message } = props;
-    return(
-        <View>
-            <Text>
-                {message};
-            </Text>
-        </View>
-    );
+    if(message != null)
+    {
+        setInterval(() => {
+            setProgress(progress + 1)
+            if(progress == 4){
+                clearInterval()
+            }
+        }, 1000);
+        return(
+            <View style={Style.container}>
+                <Text style={Style.message}>
+                    {message}
+                </Text>
+                <View style={Style.progress}/>
+            </View>
+        );
+    }
+    else{
+        return(
+        <>
+        </>
+        )
+    }
 }
 
 export default MessageErrors;
---
+++ src/components/MessageErrors/style.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+const Style = StyleSheet.create({
+    container:{
+        width: 250,
+        minHeight: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#222',
+        borderRadius: 3,
+        position: "absolute",
+        top: 40,
+        right: 10,
+        zIndex: 10,
+        padding: 10,
+        overflow: "hidden",
+    },
+    message:{
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    progress:{
+        alignSelf: "flex-start",
+        width: 250,
+        height: 5,
+        backgroundColor: "#F09E00",
+        position: "absolute",
+        bottom: 0,
+    }
+})
+export default Style;
